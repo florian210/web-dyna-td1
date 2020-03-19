54204dd794c68221c016c373f4e57c2c0b1faa84
florian210 <62303687+florian210@users.noreply.github.com>
2020-03-19 11:30:44 +0100

    Add files via upload

---
+++ ex3.php
@@ -0,0 +1,12 @@
+<form method="POST">
+	<label for="size">Size: </label>
+	<input type="number" value ="<?php echo $_POST["size"]; ?>" name="size">
+	<label for="color">color : </label>
+	<input type="string" value="" name="color">
+	<label for="message"> message: </label>
+	<input type="string" value="" name="message">
+	<textarea name="message" rows="8" cols="45" > votre message </textarea>
+	<input type="submit" value="valider">
+</form>
+
+<p style="font-size:<?php echo $_POST["size"];?>px; color:<?php echo $_POST["color"];?>"> ce texte:"<?php echo $_POST["message"];?>"est écrit en <?php echo $_POST["size"];?>
