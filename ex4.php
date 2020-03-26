8d1647e7f270e791e7d136935dfcaf63519c074c
florian210 <62303687+florian210@users.noreply.github.com>
2020-03-26 15:24:46 +0100

    Add files via upload

---
+++ ex4.php
@@ -0,0 +1,42 @@
+<?php
+$title='Ex4 ';
+?>
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+
+<hr>
+
+<?php
+$nbLignes=$_GET["li"]??10;
+$nbColonnes=$_GET["co"]??10;
+echo "<h2>Tableau de $nbLignes par $nbColonnes</h2>";
+?>
+
+<table border='1'>
+
+	<tbody>
+		<?php for($i=0;$i<$nbLignes;$i++){
+
+
+
+        $gras=($i%2==0)?"bold":"normal";
+      ?>
+
+        <tr>
+
+        <?php for($j=0;$j<$nbColonnes;$j++){
+            $couleur=($j%2==0)?"red":"black";
+		?>
+
+            <td style="font-weight: <?=$gras ?>;color:<?=$couleur ?>"><?="$i - $j"?></td>
+        <?php }?>
+        </tr>
+
+      <?php }?>
+
+    </tbody>
+</table>
+
+
+
